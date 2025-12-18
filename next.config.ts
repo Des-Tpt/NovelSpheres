780a814bd21176a0bc22a8585e9a0cf824cb8d98
Des-tpt <oopsbanana43@gmail.com>
2025-12-18 10:41:43 +0700

    hotfix after update

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ module.exports = {
       },
     ],
   },
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
--- src/app/api/user/me/route.tsx
+++ src/app/api/user/me/route.tsx
@@ -1,27 +1,31 @@
 import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/lib/auth";
 import { User } from "@/model/User";
-import { modelNames } from "mongoose";
+import { connectDB } from "@/lib/db";
 
 export async function GET() {
+  await connectDB(); 
+
   const data = await getCurrentUser();
   if (!data) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  console.log(modelNames, User);
-
-  const user = await User.findById({ _id: data._id })
+  const user = await User.findById(data._id)
     .select('_id username email role profile');
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 
   const formattedUser = {
     _id: user._id,
     username: user.username,
     email: user.email,
     role: user.role,
-    publicId: user.profile.avatar.publicId,
-    format: user.profile.avatar.format,
-  }
+    publicId: user.profile?.avatar?.publicId ?? null,
+    format: user.profile?.avatar?.format ?? null,
+  };
 
   return NextResponse.json({ user: formattedUser });
 }
